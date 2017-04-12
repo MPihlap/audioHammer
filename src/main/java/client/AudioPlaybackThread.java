a992c064c067b7c0b07d4e521aaa804781ec6157
MPihlap <meelispihlap@gmail.ee>
2017-04-12 17:20:21 +0300

    + Maven + server/client packets + .gitignore

--- src/main/java/client/AudioPlaybackThread.java
+++ src/main/java/client/AudioPlaybackThread.java
@@ -1,3 +1,5 @@
+package main.java.client;
+
 import javax.sound.sampled.*;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -1,3 +1,5 @@
+package main.java.server;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
